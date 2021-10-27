3ca801291b51f2326b4b0f6adce17ab452be03ad
aa-ag <aaron.aguerrevere@gmail.com>
2021-10-27 18:20:31 -0500

    added functions to execute queries

--- projects/movies/database.py
+++ projects/movies/database.py
@@ -1,6 +1,7 @@
 ############------------ IMPORTS ------------############
 import datetime
 import sqlite3
+from sqlite3.dbapi2 import Cursor
 
 
 ############------------ GLOBAL ------------############
@@ -9,23 +10,36 @@ connection = sqlite3.connect("data.db")
 
 ############------------ FUNCTIONS ------------############
 def create_tables():
-    pass
+    with connection:
+        connection.execute(CREATE_MOVIES_TABLE)
 
 
 def add_movie(title, release_timestamp):
-    pass
+    with connection:
+        connection.execute(INSERT_MOVIES, (title, release_timestamp))
 
 
 def get_movie(upcoming=False):
-    pass
+    with connection:
+        cursor = connection.cursor()
+        if upcoming:
+            today_timestamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+        else:
+            cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
 
 
 def watch_movie(title):
-    pass
+    with connection:
+        connection.execute(SET_MOVIE_WATCHED, (title,))
 
 
 def get_watched_movies():
-    pass
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES)
+        return cursor.fetchall()
 
 
 ############------------ QUERIES ------------############
@@ -57,3 +71,6 @@ SELECT * FROM movies WHERE release_timestamp > ?;
 SELECT_WATCHED_MOVIES = """
 SELECT * FROM movies WHERE watched = 1;
 """
+
+#  query to set watched movie
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
