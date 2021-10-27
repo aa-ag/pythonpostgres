c52dd73b194dba7286326e1a5c78ab1e3a5e265a
aa-ag <aaron.aguerrevere@gmail.com>
2021-10-27 17:31:56 -0500

    added query to insert one movie at the time into the moviest table

--- projects/movies/database.py
+++ projects/movies/database.py
@@ -1,9 +1,14 @@
 ############### QUERIES
-
+# query to create `movies` table
 CREATE_MOVIES_TABLE = """
 CREATE TABLE IF NOT EXISTS movies (
     title TEXT,
     release_timestamp REAL,
     watched INTEGER
 );
+"""
+
+# query to insert one movie into `movies` table
+INSERT_MOVIES = """
+INSERT INTO movies (title, released_timestamp, watched) VALUES (?, ?, 0);
 """
