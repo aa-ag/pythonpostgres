c3cc65f0d560347aa5ffc877b3d34a59520162f8
aa-ag <aaron.aguerrevere@gmail.com>
2021-10-28 17:40:59 -0500

    refactored print_movie_list to display human-readable dates

--- projects/movies/app.py
+++ projects/movies/app.py
@@ -19,15 +19,20 @@ Please select one of the following options:
 def prompt_add_movie():
     title = input("Movie title:  ")
     release_date = input("Release date (dd-mm-YYYY):  ")
-    parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
-    timestamp = parsed_date.timestamp()
-    db.add_movie(title, timestamp)
+    try:
+        parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+        timestamp = parsed_date.timestamp()
+        db.add_movie(title, timestamp)
+    except:
+        print("Invalid date format, please try again.")
 
 
 def print_movie_list(movies):
-    print("-- upcoming movies --")
+    print("-- upcoming movies --\n")
     for movie in movies:
-        print(f"{movie[0]} (on {movie[1]})")
+        movie_date = datetime.datetime.fromtimestamp(movie[1])
+        human_readable_date = movie_date.strftime("%b %d, %Y")
+        print(f"\"{movie[0]}\" (on {human_readable_date})")
     print("------------\n\n")
 
 
@@ -41,7 +46,8 @@ while (user_input := input(menu)) != "6":
     if user_input == "1":
         prompt_add_movie()
     elif user_input == "2":
-        pass
+        movies = db.get_movie(True)
+        print_movie_list(movies)
     elif user_input == "3":
         pass
     elif user_input == "4":
