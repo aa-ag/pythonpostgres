cecabf995e5864fbed96f867c9c259045d0621ad
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-01 20:00:41 -0500

    following along class, created method average_grade in class Student

--- refresher/oop.py
+++ refresher/oop.py
@@ -3,5 +3,8 @@ class Student:
         self.name = "Aaron"
         self.grades = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+    def average_grade(self):
+        return sum(self.grades) / len(self.grades)
+
 a_student = Student()
-print(a_student.grades)
+print(a_student.average_grade())
