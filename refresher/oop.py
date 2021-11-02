cf3ed5aec9716467ab3b797ef642e1a270523b4c
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-01 20:05:24 -0500

    modified grades to also be an input parameter in the class' __init__ method

--- refresher/oop.py
+++ refresher/oop.py
@@ -1,10 +1,10 @@
 class Student:
-    def __init__(self, s):
+    def __init__(self, s, l):
         self.name = s
-        self.grades = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.grades = l
 
     def average_grade(self):
         return sum(self.grades) / len(self.grades)
 
-a_student = Student("Aaron")
+a_student = Student("Aaron", [1, 2, 3, 4, 5, 6, 7, 8, 9])
 print(a_student.name, a_student.average_grade())
