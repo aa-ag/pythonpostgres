3cf042d433d41600b763d75568943d2ec3449ec9
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-01 20:04:26 -0500

    added name as a paramenter in the __init__ method of the class which accepts a string

--- refresher/oop.py
+++ refresher/oop.py
@@ -1,10 +1,10 @@
 class Student:
-    def __init__(self):
-        self.name = "Aaron"
+    def __init__(self, s):
+        self.name = s
         self.grades = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     def average_grade(self):
         return sum(self.grades) / len(self.grades)
 
-a_student = Student()
-print(a_student.average_grade())
+a_student = Student("Aaron")
+print(a_student.name, a_student.average_grade())
