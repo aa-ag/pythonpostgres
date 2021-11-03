671be7e0041a959ff3781f2cb019b84fae37d765
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-02 20:23:52 -0500

    added __repr__ magic method to Person class for debugger

--- refresher/oop.py
+++ refresher/oop.py
@@ -3,8 +3,11 @@ class Person:
         self.name = name
         self.age = age
 
-    def __str__(self):
-        return f"Person: {self.name}; age: {self.age} years old."
+    #def __str__(self):
+    #    return f"Person: {self.name}; age: {self.age} years old."
+
+    def __repr__(self):
+        return f"<Person('{self.name}', {self.age})>"
 
 a_person_named_bob_who_is_35 = Person("Bob", 35)
 print(a_person_named_bob_who_is_35)
