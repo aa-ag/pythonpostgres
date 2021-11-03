4791e5c1dca6fcd1a74281a70bae00d49efdbe75
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-02 20:38:15 -0500

    added docstrings inside each type of class method in oop.py

--- refresher/oop.py
+++ refresher/oop.py
@@ -1,13 +1,24 @@
 class ClassTest:
     def instace_method(self):
+        '''
+         used for most things: produce an action 
+         with the data inside an object, or if you
+         want to modify the data inside an object
+        '''
         print(f"Called instance_method of {self}")
 
     @classmethod
     def class_method(cls):
+        '''
+         commonly used as factories
+        '''
         print(f"Called class_method of {cls}")
 
     @staticmethod
     def static_method():
+        '''
+         used to place a method inside a class
+        '''
         print("Called static_method.")
 
 
