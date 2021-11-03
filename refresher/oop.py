ed7bbc330cb048e14560e4da4ea2697ac067ac2a
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-02 20:28:21 -0500

    added instance_method and two options for calling it in ClassTest

--- refresher/oop.py
+++ refresher/oop.py
@@ -1,13 +1,11 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+class ClassTest:
+    def instace_method(self):
+        print(f"Called instance_method of {self}")
 
-    #def __str__(self):
-    #    return f"Person: {self.name}; age: {self.age} years old."
+test = ClassTest()
 
-    def __repr__(self):
-        return f"<Person('{self.name}', {self.age})>"
+# option 1
+test.instace_method()
 
-a_person_named_bob_who_is_35 = Person("Bob", 35)
-print(a_person_named_bob_who_is_35)
+# option 2 (exactly the same)
+ClassTest.instace_method(test)
