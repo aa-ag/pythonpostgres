ddd6297c3c6da28f0658b35b7f67277467af01ef
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-03 18:49:11 -0500

    added inheritance.py and Device class from class

---
+++ refresher/inheritance.py
@@ -0,0 +1,16 @@
+class Device:
+    def __init__(self, name, connected_by):
+        self.name = name
+        self.connected_by = connected_by
+        self.connected = True
+
+    def __str__(self):
+        return f"Device {self.name!r} ({self.connected_by})"
+
+    def disconnect(self):
+        self.connected = False
+        print("Device disconnected")
+
+printer = Device("Printer", "USB")
+print(printer)
+printer.disconnect()
