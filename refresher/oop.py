512e1d8b4b0ac757cf7a3dfb64ece0e2911cec0c
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-02 20:17:52 -0500

    printing the string representing the created object

--- refresher/oop.py
+++ refresher/oop.py
@@ -1,10 +1,20 @@
-class Student:
-    def __init__(self, s, l):
-        self.name = s
-        self.grades = l
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    def average_grade(self):
-        return sum(self.grades) / len(self.grades)
+a_person_named_bob_who_is_35 = Person("Bob", 35)
+print(a_person_named_bob_who_is_35)
 
-a_student = Student("Aaron", [1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(a_student.name, a_student.average_grade())
+
+
+# class Student:
+#     def __init__(self, s, l):
+#         self.name = s
+#         self.grades = l
+
+#     def average_grade(self):
+#         return sum(self.grades) / len(self.grades)
+
+# a_student = Student("Aaron", [1, 2, 3, 4, 5, 6, 7, 8, 9])
+# print(a_student.name, a_student.average_grade())
