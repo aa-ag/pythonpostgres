44b99ebe8adb4be07cf506c0f851989ad40b0a9e
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-07 19:36:23 -0600

    added Book class from class to show example of how inheritance does not work for every case, as sometimes a parent class, if inherited, doesn't fit the object well:

--- refresher/composition.py
+++ refresher/composition.py
@@ -6,6 +6,12 @@ class Bookshelf:
         return f"Bookshelf with {self.quantity} books."
 
 
+class Book:
+    def __init__(self, name, quantity):
+        super().__init__(quantity)
+        self.name = name
+
+
 ###### DRIVER CODE ######
 shelf = Bookshelf(300)
 print(shelf)
