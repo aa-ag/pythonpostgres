16df10bfaf4d87f1703bbe2561dbf60406fbdf0f
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-12 16:46:42 -0600

    added type hinting logic to classmethods in Book class

--- refresher/oop.py
+++ refresher/oop.py
@@ -35,17 +35,17 @@ class Book:
         return f"<Book {self.name}, {self.type}, wighing {self.weight}g>"
 
     @classmethod
-    def hardcover(cls, name, weight):
+    def hardcover(cls, name: str, weight: int) -> "Book":
         return cls(name, cls.TYPES[0], weight + 100)
 
     @classmethod
-    def paperback(cls, name, weight):
+    def paperback(cls, name: str, weight: int) -> "Book":
         return cls(name, cls.TYPES[1], weight)
 
 
 a_book = Book.hardcover("Harry Potter", 1500)
 another_book = Book.paperback("Harry Potter", 200)
-a_book_with_wrong_inputs = Book.hardcover(1, 2, '3')
+# a_book_with_wrong_inputs = Book.hardcover(1, 2, '3')
 
 print(a_book)
 print(another_book)
