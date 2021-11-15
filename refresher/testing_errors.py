26d5e9a584cabdd691e06632710f4b4498c1587e
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-14 19:14:31 -0600

    added ZeroDivisionError in divide function

--- refresher/testing_errors.py
+++ refresher/testing_errors.py
@@ -1,13 +1,13 @@
 def divide(dividend, divisor):
     if divisor == 0:
-        print("divisor cannot be 0")
+        raise ZeroDivisionError("divisor cannot be 0")
         return 
     return dividend / divisor
 
 divide(10, 0)
 
 
-grades = list()
+# grades = list()
 
-average = divide(sum(grades), len(grades))
-print(average)
+# average = divide(sum(grades), len(grades))
+# print(average)
