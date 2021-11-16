debca4cbac106ecb3f0d6708f7eac73ea626f40f
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-16 17:05:23 -0600

    added a search function with the intention of having it execute a given search method inside each element of the sequence, yet to be defined

--- refresher/first_class_functions.py
+++ refresher/first_class_functions.py
@@ -1,11 +1,5 @@
-def divide(dividend, divisor):
-    if divisor == 0:
-        raise ZeroDivisionError("divisor cannot be 0") 
-    return dividend / divisor
-
-
-def calculate(*values, operator):
-    return operator(*values)
-
-result = calculate(10, 2, operator=divide)
-print(result)
+def search(sequence, target, search_method):
+    for i in sequence:
+        if search_method(i) == target:
+            return f"Yes, {i} is found in provided sequence"
+    raise RuntimeError(f"Unable to find {target}")
