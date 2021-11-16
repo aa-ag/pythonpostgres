f2e232a5713a8a6e50fadd5125d17a9e1d06bdfc
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-16 17:09:35 -0600

    added driver code & test case and refactored return statement for search function;  working as expected

--- refresher/first_class_functions.py
+++ refresher/first_class_functions.py
@@ -1,9 +1,17 @@
 def search(sequence, target, search_method):
     for i in sequence:
         if search_method(i) == target:
-            return f"Yes, {i} is found in provided sequence"
+            return f'Yes, \"{i["name"]}\" is found in provided sequence'
     raise RuntimeError(f"Unable to find {target}")
 
 
 def iterate_over_friends_object(friend):
     return friend["name"]
+
+friends = [
+    {"name": "a", "age": 1},
+    {"name": "b", "age": 2},
+    {"name": "c", "age": 3},
+]
+
+print(search(friends, "c", iterate_over_friends_object))
