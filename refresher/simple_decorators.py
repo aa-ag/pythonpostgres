270aec1dcb5e435f4dcd46ac69be866b612e2a12
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-17 20:19:22 -0600

    refactored script using code from class to use simple decorator logic

--- refresher/simple_decorators.py
+++ refresher/simple_decorators.py
@@ -1,16 +1,12 @@
-all_users = [
-    {"user": "a","access_level": "guest"},
-    {"user": "b","access_level": "admin"}
-]
+user = {"user": "a","access_level": "guest"}
 
 def get_admin_password():
     return "1234"
 
-def check_access_level():
-    for a_user in all_users:
-        if a_user["access_level"] != "admin":
-            print(f'user {a_user["user"]} only has \"{a_user["access_level"]}\" level access')
-        else:
-            print(f'{a_user["user"]}\'s admin password is: {get_admin_password()}')
+def check_access_level(function):
+    if user["access_level"] == "admin":
+        return function
 
-check_access_level()
+
+get_admin_password = check_access_level(get_admin_password)
+print(get_admin_password)
