cee4ccb716842481c76b76674848aa3c6c68eaf7
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-17 20:13:18 -0600

    added long version of intended logic to check access level for users

--- refresher/simple_decorators.py
+++ refresher/simple_decorators.py
@@ -1,9 +1,16 @@
-user = {
-    "user": "a",
-    "access_level": "guest"
-}
+all_users = [
+    {"user": "a","access_level": "guest"},
+    {"user": "b","access_level": "admin"}
+]
 
 def get_admin_password():
     return "1234"
 
-print(get_admin_password())
+def check_access_level():
+    for a_user in all_users:
+        if a_user["access_level"] != "admin":
+            print(f'user {a_user["user"]} only has \"{a_user["access_level"]}\" level access')
+        else:
+            print(f'{a_user["user"]}\'s admin password is: {get_admin_password()}')
+
+check_access_level()
