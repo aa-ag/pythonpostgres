2c25473282ebe55eacad24b01718882c22d82d09
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-18 20:37:23 -0600

    refactored get_admin_password to have an @ decorator of make_function_secure which necessitated that make_function_secure be above get_admin_password

--- refresher/decorators.py
+++ refresher/decorators.py
@@ -1,6 +1,3 @@
-def get_admin_password():
-    return "1234"
-
 def make_function_secure(function):
     def secure_function():
         if user["access_level"] == "admin":
@@ -11,6 +8,11 @@ def make_function_secure(function):
     return secure_function
 
 
-get_admin_password = make_function_secure(get_admin_password)
+@make_function_secure
+def get_admin_password():
+    return "1234"
+    
+
+get_admin_password = get_admin_password()
 user = {"user": "a","access_level": "admin"}
 print(get_admin_password())
