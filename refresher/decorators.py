0bfccdc22d4cb41dadeab657f41f9bb26d8c4e36
aa-ag <aaron.aguerrevere@gmail.com>
2021-11-19 21:58:41 -0600

    refactored to make names more appropriate per their jobs/what they do

--- refresher/decorators.py
+++ refresher/decorators.py
@@ -2,8 +2,8 @@ import functools as ft
 
 user = {"user": "a","access_level": "guest"}
 
-def decorator(access_level):
-    def make_function_secure(function):
+def make_function_secure_factory(access_level):
+    def make_function_secure_decorator(function):
         @ft.wraps(function)
         def secure_function(*args, **kwargs):
             if user["access_level"] == "admin":
@@ -12,17 +12,17 @@ def decorator(access_level):
                 return "user has insufficient access level"
         
         return secure_function
-    return make_function_secure
+    return make_function_secure_decorator
 
 
-@make_function_secure
+@make_function_secure_factory
 def get_admin_password(panel):
     if panel == "admin":
         return "1234"
     return "nope"
 
 
-@make_function_secure
+@make_function_secure_factory
 def get_dashboard_password(panel):
     return "user: userpassword"
 
