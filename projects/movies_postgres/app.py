6474b73b950944957ccb51302dbc992e984c3853
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-01 18:42:56 -0600

    added global variable url from elephantsql per class

--- projects/movies_postgres/app.py
+++ projects/movies_postgres/app.py
@@ -2,6 +2,7 @@
 import psycopg2
 
 ############------------ GLOBAL VARIABLE(S) ------------############
+url = 'postgres://qfdrqgjd:IdD-59wdGyZkCM6l8FRAZTCbs646JQK6@hansken.db.elephantsql.com/qfdrqgjd'
 
 
 ############------------ FUNCTION(S) ------------############
