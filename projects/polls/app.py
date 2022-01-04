88dc26720ae4b31c2b6ce90761b1f04a1e5ac94f
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-04 13:27:51 -0600

    added base structure & imports

---
+++ projects/polls/app.py
@@ -0,0 +1,14 @@
+############------------ IMPORTS ------------############
+import psycopg2
+from psycopg2.errors import DivisionByZero
+import database
+import settings
+
+
+############------------ GLOBAL VARIABLE(S) ------------############
+
+
+############------------ FUNCTION(S) ------------############
+
+
+############------------ DRIVER CODE ------------############
