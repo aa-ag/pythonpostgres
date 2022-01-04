5bf240267e281ff3cfea85dd415e2c82fa6b54c5
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-03 21:07:19 -0600

    added CREATE_OPTIONS query

--- projects/polls/database.py
+++ projects/polls/database.py
@@ -9,3 +9,9 @@ CREATE_POLLS = """
 CREATE TABLE IF NOT EXISTS polls 
 (id SERIAL PRIMARY_KEY, title TEXT, owner_username TEXT)';
 """
+
+CREATE_OPTIONS = """
+CREATE TABLE IF NOT EXISTS options
+(id SERIAL PRIMARY_KEY, option_text TEXT, poll_id INTEGER, 
+FOREIGN_KEY(poll_id) REFERENCES polls (id));
+"""
