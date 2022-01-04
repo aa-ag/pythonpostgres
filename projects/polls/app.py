ca825d8d45d077015739d40bd2447e8dd6d65251
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-04 13:30:56 -0600

    added MENU_PROMPT

--- projects/polls/app.py
+++ projects/polls/app.py
@@ -6,7 +6,17 @@ import settings
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
+MENU_PROMPT = """
+-- MENU --
+(1) create a poll
+(2) list open polls
+(3) vote on a poll
+(4) show poll votes
+(5) select a random winner from a poll option
+(6) exit
 
+Enter your choice:
+"""
 
 ############------------ FUNCTION(S) ------------############
 
