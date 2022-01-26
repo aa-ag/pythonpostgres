7f23328f37b5b1dd3759a239816b7d69d2537a7e
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-26 17:47:06 -0600

    Replaced get_connection with pool.getconn inside each function

--- projects/polls/option.py
+++ projects/polls/option.py
@@ -13,26 +13,26 @@ class Option:
         return f"Option {self.text!r}, {self.poll_id!r}, {self.id!r}"
 
     def save(self):
-        connection = create_connection()
+        connection = pool.getconn()
         new_option_id = database.add_option(connection, option_id)
         connection.close()
         self.id = new_option_id
 
     @classmethod
     def get(cls, option_id: int) -> "Option":
-        connection = create_connection()
+        connection = pool.getconn()
         option = database.get_option(connection, option_id)
         connection.close()
         return cls(option[1], option[2], option[0])
 
     def vote(self, username: str):
-        connection = create_connection()
+        connection = pool.getconn()
         database.add_poll_vote(connection, self.id)
         connection.close()
 
     @property
     def votes(self) -> List[database.Vote]:
-        connection = create_connection()
+        connection = pool.getconn()
         votes = database.get_votes_for_options(connection, self.id)
         connection.close()
         return votes
