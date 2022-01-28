19d93d652ceb368c75470b113a08ebb7a7953d66
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-27 21:37:44 -0600

    Add contextmanager import from contextlib

--- projects/polls/connection_pool.py
+++ projects/polls/connection_pool.py
@@ -1,5 +1,6 @@
 import os
 from psycopg2.pool import SimpleConnectionPool
 from settings import postgres_url
+from contextlib import contextmanager
 
 pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=postgres_url)
