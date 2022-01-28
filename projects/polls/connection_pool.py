621c04ca57c4e3078c5bed1570aefcb6b553a60c
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-27 21:39:32 -0600

    Add try/finally statement to create, yield and put connection

--- projects/polls/connection_pool.py
+++ projects/polls/connection_pool.py
@@ -1,4 +1,5 @@
 import os
+from sqlite3 import connect
 from psycopg2.pool import SimpleConnectionPool
 from settings import postgres_url
 from contextlib import contextmanager
@@ -7,4 +8,8 @@ pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=postgres_url)
 
 @contextmanager
 def get_connection():
-    pass
+    connection = pool.getconn()
+    try:
+        yield connection
+    finally:
+        pool.putconn(connection)
