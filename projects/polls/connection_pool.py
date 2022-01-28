96685213c7934ae8b48f128749ecb01d45e45dcb
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-27 21:38:27 -0600

    Add get_connection function using the contextmanager decorator

--- projects/polls/connection_pool.py
+++ projects/polls/connection_pool.py
@@ -4,3 +4,7 @@ from settings import postgres_url
 from contextlib import contextmanager
 
 pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=postgres_url)
+
+@contextmanager
+def get_connection():
+    pass
