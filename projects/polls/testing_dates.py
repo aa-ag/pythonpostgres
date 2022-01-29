f75208158cb9782db7c8a5e6bd1816348602d947
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-28 22:16:11 -0600

    Add try-except for user provided timezone

--- projects/polls/testing_dates.py
+++ projects/polls/testing_dates.py
@@ -2,3 +2,9 @@ import pytz
 import json
 
 user_timezone = input("Enter your timezone: ").strip()
+
+try:
+    pytz.timezone(user_timezone)
+except pytz.exceptions.UnknownTimeZoneError:
+    print("Unkown Time Zone.")
+    raise
