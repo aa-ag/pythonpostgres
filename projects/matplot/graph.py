b41d90ab9a04c2c5a273507a0b3589e744fcb98d
aa-ag <aaron.aguerrevere@gmail.com>
2022-02-06 10:28:53 -0600

    Add pie.py's logic to create a test pie chart with three groups

--- projects/matplot/graph.py
+++ projects/matplot/graph.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 plt.figure()
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.title('test')
 lines = plt.plot([1, 2, 3, 4], [3, 5, 7, 9])
 plt.setp(lines, color='00000')
 plt.show()
